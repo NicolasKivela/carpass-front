d59b9098e918208b8bf9a793b55d27b838b55cdc
Joona <joona.v.korkeamaki@tuni.fi>
2024-01-18 13:47:38 +0200

    Added React-Native-Paper and Vector-Icons

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -4,14 +4,34 @@ import {useTranslation} from 'react-i18next';
 
 import {APP_NAME} from '../common/constanst';
 import {colors} from '../common/styles';
+import {Button, PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const LoginScreen: React.FC = () => {
   const {t} = useTranslation();
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.text}>{APP_NAME}</Text>
-      <Text style={styles.text}>{t('login')}</Text>
-    </ScrollView>
+    <PaperProvider>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text}>{APP_NAME}</Text>
+        <Text style={styles.text}>{t('login')}</Text>
+
+        <Button
+          mode="outlined"
+          buttonColor={colors.black}
+          onPress={() => console.log('Pressed')}
+          icon={props => (
+            <MaterialIcons
+              {...props}
+              name="login"
+              size={20}
+              color={colors.orange}
+            />
+          )}>
+          Press Me
+        </Button>
+      </ScrollView>
+    </PaperProvider>
   );
 };
 
