342c8653a7a48510bc1c923ad375eecf5e97b777
aliisanissinen <aliisa.nissinen@hotmail.com>
2024-03-05 09:37:16 +0200

    Fix dropdownNotification

--- src/components/dropdownNotification/index.tsx
+++ src/components/dropdownNotification/index.tsx
@@ -1,5 +1,4 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
 import DropdownAlert, {
   DropdownAlertType,
   DropdownAlertData,
@@ -7,62 +6,56 @@ import DropdownAlert, {
 import timer from 'react-native-timer';
 
 import {useAppDispatch, useAppSelector} from '../../store/configureStore';
-
+import {deleteError} from '../../store/actions/error';
 import {colors} from '../../common/styles';
 
-import styles from './styles';
+import {styles} from './styles';
 
-interface Props {}
+const INTERVAL = 5000;
 
-const DropdownNotification: React.FC<Props> = ({}) => {
+const DropdownNotification: React.FC = ({}) => {
   const dispatch = useAppDispatch();
-  const error = useAppSelector(state => state.errors.error); //TODO add
+  const error = useAppSelector(state => state.error);
 
-  const alert = useRef(
-    (_data: DropdownAlertData) => new Promise<DropdownAlertData>(res => res),
-  );
-
-  const [showError, setShowError] = useState(false);
+  let alert = (_data: DropdownAlertData) =>
+    new Promise<DropdownAlertData>(res => res);
 
   useEffect(() => {
     return () => {
       timer.clearTimeout('errorNotification');
+      dispatch(deleteError());
     };
   }, []);
 
   useEffect(() => {
-    setShowError(true);
-    alert.current({
-      type: DropdownAlertType.Error,
-      title: error.title,
-      message: error.message,
-    });
-    timer.setTimeout(
-      'errorNotification',
-      () => {
-        setShowError(false);
-        dispatch(deleteError());
-      },
-      5000,
-    );
-  }, [error]);
+    if (error.message && error.title) {
+      alert({
+        type: DropdownAlertType.Error,
+        title: error.title,
+        message: error.message,
+      });
 
-  // const renderMessage = () => {
-  //   return (
-  //     <View>
-  //       <Text style={styles.errorText}>{alert.current}</Text>
-  //     </View>
-  //   );
-  // };
+      timer.setTimeout(
+        'errorNotification',
+        () => {
+          dispatch(deleteError());
+        },
+        INTERVAL,
+      );
+    }
+  }, [error]);
 
   return (
     <DropdownAlert
-      alert={func => (alert.current = func)}
-      onDismissAutomatic={() => dispatch(deleteError())} //TODO
+      alert={func => (alert = func)}
+      dismissInterval={INTERVAL}
+      onDismissAutomatic={() => dispatch(deleteError())}
+      errorColor={colors.orange}
+      alertViewStyle={styles.errorContainer}
       elevation={10}
       zIndex={10}
       activeStatusBarBackgroundColor={colors.orange}
-      //renderMessage={renderMessage}
+      inactiveStatusBarBackgroundColor={colors.black}
     />
   );
 };
