309f7bdb2a09f2cfb716bcecbc98ff541b8d9717
NicolasKivela <nicolas.kivela@gmail.com>
2024-05-08 20:05:45 +0300

    Inputs added to description component

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
+import {TextInput} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
 
 import {TextField, PictureAddition} from '../index';
@@ -22,13 +23,30 @@ const Description: React.FC<ModalContentProps> = ({visible, id}) => {
   const report_rows = useAppSelector(state => state.report.report_rows);
 
   const [text, setText] = useState('');
+  const [questionType, setQuestionType] = useState('');
+  const [singleNumeric, setSingleNumeric] = useState(null);
+  const [leftInput, setLeftInput] = useState(null);
+  const [rightInput, setRightInput] = useState(null);
   const {t} = useTranslation();
 
   useEffect(() => {
-    const comment = report_rows.find(
+    const reportRow = report_rows.find(
       (item: ReportRow) => item.question_id === id,
-    )?.comment;
-    comment && setText(comment);
+    );
+    if (reportRow) {
+      const {type, additional_input, comment, input_left, input_right} =
+        reportRow;
+      setQuestionType(type);
+      // Set single numeric value if additional input exists
+      additional_input && setSingleNumeric(additional_input);
+
+      // Set text if comment exists
+      comment && setText(comment);
+
+      // Set left and right inputs
+      input_left && setLeftInput(input_left);
+      input_right && setRightInput(input_right);
+    }
   }, []);
 
   if (!visible) {
@@ -37,17 +55,26 @@ const Description: React.FC<ModalContentProps> = ({visible, id}) => {
 
   return (
     <View style={styles.view}>
-      <TextField
-        secureTextEntry={false}
-        label={t('description')}
-        onChangeText={value => {
-          setText(value);
-          dispatch(setReportRowComment(id, value));
-        }}
-        value={text}
-        //rightIcon="keyboard-voice"
-        //onIconPress={() => console.log('click')}
-      />
+      {questionType === 'description' && (
+        <TextField
+          secureTextEntry={false}
+          label={t('description')}
+          onChangeText={value => {
+            setText(value);
+            dispatch(setReportRowComment(id, value));
+          }}
+          value={text}
+          //rightIcon="keyboard-voice"
+          //onIconPress={() => console.log('click')}
+        />
+      )}
+      {questionType === 'leftrightnumeric' && (
+        <View>
+          <TextInput />
+          <TextInput />
+        </View>
+      )}
+      {questionType === 'singlenumeric' && <TextInput />}
       <PictureAddition id={id} />
     </View>
   );
